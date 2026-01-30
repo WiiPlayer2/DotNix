99cb2cee474442b90fd632346973558ef3a47715
Waldemar Tomme <waldetomme@gmail.com>
2026-01-30 21:52:45 +0100

    Cleanup builtins

--- DotNix/Compiling/Builtins.cs
+++ DotNix/Compiling/Builtins.cs
@@ -5,20 +5,45 @@ public static class Builtins
     public static NixAttrs AsAttrs => field ??= new NixAttrs(Map<string, NixValue2>(
         ("add", AddFn)
     ));
+    
+    private static NixFunction AddFn => OfType<NixNumber, NixNumber>(Add);
 
-    private static NixFunction AddFn => field ??= new(a => Task.FromResult<NixValue2>(new NixFunction(async b => Add((NixNumber) await a.Strict, (NixNumber) await b.Strict))));
+    private static NixFunction OfType<A, B>(Func<A, B, NixValue2> fn)
+        where A : NixValue2
+        where B : NixValue2
+        => OfType((A a) => OfType((B b) => fn(a, b)));
+    
+    private static NixFunction OfType<A>(Func<A, NixValue2> fn)
+        where A : NixValue2
+        => new(async arg =>
+            (await arg.Strict) is not A argTyped
+                ? throw new NotSupportedException()
+                : fn(argTyped)
+        );
     
     public static NixNumber Add(NixNumber a, NixNumber b) =>
-        CalcOp(Add, Add, a, b);
+        CalcOp(
+            (a1, b1) => new(a1.Value + b1.Value),
+            (a1, b1) => new(a1.Value + b1.Value),
+            a, b);
 
     public static NixNumber Sub(NixNumber a, NixNumber b) =>
-        CalcOp(Sub, Sub, a, b);
+        CalcOp(
+            (a1, b1) => new(a1.Value - b1.Value),
+            (a1, b1) => new(a1.Value - b1.Value),
+            a, b);
     
     public static NixNumber Mul(NixNumber a, NixNumber b) =>
-        CalcOp(Mul, Mul, a, b);
+        CalcOp(
+            (a1, b1) => new(a1.Value * b1.Value),
+            (a1, b1) => new(a1.Value * b1.Value),
+            a, b);
 
     public static NixNumber Div(NixNumber a, NixNumber b) =>
-        CalcOp(Div, Div, a, b);
+        CalcOp(
+            (a1, b1) => new(a1.Value / b1.Value),
+            (a1, b1) => new(a1.Value / b1.Value),
+            a, b);
 
     private static NixNumber CalcOp(Func<NixInteger, NixInteger, NixInteger> integerFn, Func<NixFloat, NixFloat, NixFloat> floatFn, NixNumber a, NixNumber b) =>
         a is NixInteger aInt
@@ -28,20 +53,4 @@ public static class Builtins
             : b is NixInteger bInt2
                 ? floatFn((NixFloat) a, bInt2.AsFloat)
                 : floatFn((NixFloat) a, (NixFloat) b);
-    
-    public static NixInteger Add(NixInteger a, NixInteger b) => new(a.Value + b.Value);
-    
-    public static NixInteger Sub(NixInteger a, NixInteger b) => new(a.Value - b.Value);
-    
-    public static NixInteger Mul(NixInteger a, NixInteger b) => new(a.Value * b.Value);
-    
-    public static NixInteger Div(NixInteger a, NixInteger b) => new(a.Value / b.Value);
-    
-    public static NixFloat Add(NixFloat a, NixFloat b) => new(a.Value + b.Value);
-    
-    public static NixFloat Sub(NixFloat a, NixFloat b) => new(a.Value - b.Value);
-    
-    public static NixFloat Mul(NixFloat a, NixFloat b) => new(a.Value * b.Value);
-    
-    public static NixFloat Div(NixFloat a, NixFloat b) => new(a.Value / b.Value);
 }
