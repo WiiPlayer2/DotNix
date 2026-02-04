8e44ef06511968423b4f26ddef9c3c7763e5fc21
Waldemar Tomme <waldetomme@gmail.com>
2026-02-04 13:05:27 +0100

    Fix devShell for aarch64

--- nix/modules/devShell.nix
+++ nix/modules/devShell.nix
@@ -5,7 +5,9 @@
       devShells.default = pkgs.mkShell {
         name = "dotnix";
 
-        packages = with pkgs; with self'.legacyPackages; [
+        packages = with pkgs; [
+          dotnet-sdk_10
+          omnisharp-roslyn
         ];
       };
     };
